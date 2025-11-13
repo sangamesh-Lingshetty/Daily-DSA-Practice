90e3b19d74b59d8f8810850c24290a03572f8bcb
Sangamesh <132161994+sangamesh-Lingshetty@users.noreply.github.com>
2025-11-14 00:14:58 +0530

    changes is added the shift,unshift,getItem with optimization solution

--- linkdlist/doublyLinkdList.js
+++ linkdlist/doublyLinkdList.js
@@ -36,13 +36,13 @@ class doublyLinkdList {
     return this;
   }
 
-  pop(){
+  pop() {
     let removedNode = this.tail;
-    if(!this.head) return null || undefined;
-    if(this.length ===1){
+    if (!this.head) return null || undefined;
+    if (this.length === 1) {
       this.head = null;
       this.tail = null;
-    }else{
+    } else {
       this.tail = removedNode.prev;
       this.tail.next = null;
       removedNode.prev = null;
@@ -50,13 +50,70 @@ class doublyLinkdList {
     this.length--;
     return removedNode;
   }
+
+  shift() {
+    //remove the first elements.
+    let removedNode = this.head;
+    if (!this.head) return null || undefined;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removedNode.next;
+      this.head.prev = null;
+      removedNode.next = null;
+    }
+    this.length--;
+    return removedNode;
+  }
+
+  unshift(value) {
+    let newnode = new Node(value);
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+    } else {
+      this.head.prev = newnode;
+      newnode.next = this.head;
+      this.head = newnode;
+    }
+    this.length++;
+    return this;
+  }
+
+  getItem(index) {
+   
+    let midvalue = this.length / 2;
+    if (midvalue > index) {
+      let current = this.head;
+      for(let i =0;i<index;i++){
+        current = current.next;
+      }
+      return current;
+    } else {
+      let current = this.tail;
+      for(let i =this.length -1;i>0;i--){
+        current = current.prev;
+      }
+      return current;
+    }
+  }
 }
 
 // 10 -> 100 -> 200
-    //<-
+//<-
 const values = new doublyLinkdList();
-values.insert(23);
+values.insert(123);
 values.insert(100);
 values.insert(200);
-values.pop()
-console.log(values);
+values.insert(300);
+values.insert(400);
+values.insert(500);
+values.insert(600);
+values.insert(700);
+values.insert(800);
+// values.pop();
+// values.shift() remove from the first value...
+// values.unshift(0); //add the from the biggining....
+
+console.log(values.getItem(4));
