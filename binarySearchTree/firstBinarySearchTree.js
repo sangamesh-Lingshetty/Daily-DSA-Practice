d7ab43f32fe1185e7f28ff19f22828635b0eb245
Sangamesh <132161994+sangamesh-Lingshetty@users.noreply.github.com>
2025-11-25 23:45:16 +0530

    added the insert and find the value from tree

--- binarySearchTree/firstBinarySearchTree.js
+++ binarySearchTree/firstBinarySearchTree.js
@@ -1,20 +1,74 @@
-class BinarySearchTree{
-    constructor(){
-        this.root = null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-class Node{
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
+  insertNewNode(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (current.value === value) return null;
+        if (current.value > value) {
+          //left part
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (current.value < value) {
+          //right part.
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+    return this;
+  }
+
+  findValue(findValue) {
+    if (!this.root) {
+      return null || undefined;
+    }
+    if (this.root.value === findValue) return true;
+    let current = this.root;
+    let found = false;
+    while (current) {
+      if (current.value > findValue) {
+        current = current.left;
+      } else if (current.value === findValue) {
+        return true;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+}
 
 let tree = new BinarySearchTree();
-tree.root = new Node(34);
-tree.root.left = new Node(23);
-tree.root.right = new Node(45);
-tree.root.left.left = new Node(12);
+tree.insertNewNode(10);
+tree.insertNewNode(6);
+tree.insertNewNode(3);
+tree.insertNewNode(8);
+tree.insertNewNode(15);
+tree.insertNewNode(20);
+tree.insertNewNode(13);
+// tree.findValue(3);
+
+console.log(tree.findValue(1056));
